e58ddea072a3a021a1046ba29add05da54210008
David Baba <davidbabasalifu2020@gmail.com>
2021-02-28 00:59:04 -1200

    The second commit

---
+++ routes/authors.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+//All AUthors Route
+router.get('/', (req, res) =>{
+  res.render('authors/index')
+})
+
+//New Author Route
+router.get('/new', (req, res) =>{
+  res.render('authors/new', {author: new Author()})
+})
+
+//Create Author Route
+router.post('/', async (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+  try {
+    const newAuthor = await author.save()
+    // res.redirect(`authors/${newAuthor.id}`)
+      res.redirect(`authors`)
+  } catch {
+    res.render('authors/new',{
+      author: author,
+      errorMessage: 'Error creating author'
+   })
+ }
+}) 
+
+module.exports = router
